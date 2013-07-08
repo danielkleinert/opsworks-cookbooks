3bfb2706ba50d8c4ffbc0909f650d9650f0e0385
Daniel Kleinert <herr.kleinert@googlemail.com>
2013-07-08 16:26:06 +0200

    undefined method `default_action' for #<Class:0x7f67e3f2f2e0>

--- mongodb/resources/user.rb
+++ mongodb/resources/user.rb
@@ -1,5 +1,6 @@
 actions :add, :delete, :update
-default_action :add
+#undefined method `default_action' for #<Class:0x7f67e3f2f2e0>
+#default_action :add
 
 attribute :name, :name_attribute => true, :kind_of => String, :required => true
 attribute :password, :kind_of => String, :required => true

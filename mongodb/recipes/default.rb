bb00ac7949b9b9c273ed364f4f4a268290099230
Daniel Kleinert <herr.kleinert@googlemail.com>
2013-07-08 18:54:11 +0200

    hmm

--- mongodb/recipes/default.rb
+++ mongodb/recipes/default.rb
@@ -24,8 +24,18 @@ package node[:mongodb][:package_name] do
   version node[:mongodb][:package_version]
 end
 
-chef_gem 'bson_ext'
-chef_gem 'mongo'
+#chef_gem 'bson_ext'
+#chef_gem 'mongo'
+
+gem_package 'bson_ext' do
+  action :nothing
+end.run_action(:install)
+Gem.clear_paths
+
+gem_package 'mongo' do
+  action :nothing
+end.run_action(:install)
+Gem.clear_paths
 
 if node.recipe?("mongodb::default") or node.recipe?("mongodb")
   # configure default instance

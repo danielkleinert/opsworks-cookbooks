e33f34d765f0cc1ea55a81f04439b38d776f417d
Daniel Kleinert <herr.kleinert@googlemail.com>
2013-07-09 13:35:15 +0200

    using upstart

--- mongodb/attributes/default.rb
+++ mongodb/attributes/default.rb
@@ -77,6 +77,7 @@ else
   default[:mongodb][:package_name] = "mongodb-10gen"
   default[:mongodb][:apt_repo] = "debian-sysvinit"
 
+
 end
 
 default[:mongodb][:package_version] = nil
--- test/attributes/defaults.rb
+++ test/attributes/defaults.rb
@@ -1,3 +1,4 @@
 set[:mongodb][:cluster_name] = "test_cluster"
 set[:mongodb][:replicaset_name] = "test_repl"
 #set[:mongodb][:replicaset_members] = 
+set[:mongodb][:apt_repo] = "ubuntu-upstart"

77fe616f90696417b288564de3ef1e97944126f2
Daniel Kleinert <herr.kleinert@googlemail.com>
2013-07-10 16:05:06 +0200

    qq

--- test/recipes/arbiter.rb
+++ test/recipes/arbiter.rb
@@ -1,15 +1,15 @@
 
-set[:mongodb][:cluster_name] = node[:opsworks][:stack][:name]
-set[:mongodb][:replicaset_name] = "rs_#{node[:opsworks][:stack][:name]}"
+node.set[:mongodb][:cluster_name] = node[:opsworks][:stack][:name]
+node.set[:mongodb][:replicaset_name] = "rs_#{node[:opsworks][:stack][:name]}"
 if node[:opsworks][:layers][:mongo] != 0 and not node[:opsworks][:layers][:mongo][:instances].empty?
-	set[:mongodb][:replicaset_members] = node[:opsworks][:layers][:mongo][:instances].values.map{ |instance| instance[:private_dns_name]}
+	node.set[:mongodb][:replicaset_members] = node[:opsworks][:layers][:mongo][:instances].values.map{ |instance| instance[:private_dns_name]}
 end
 if node[:opsworks][:layers][:arbiter] != 0 and not node[:opsworks][:layers][:arbiter][:instances].empty?
-	set[:mongodb][:replicaset_arbiters] = node[:opsworks][:layers][:arbiter][:instances].values.map{ |instance| instance[:private_dns_name]}
+	node.set[:mongodb][:replicaset_arbiters] = node[:opsworks][:layers][:arbiter][:instances].values.map{ |instance| instance[:private_dns_name]}
 end
-set[:mongodb][:smallfiles] = true
-#set[:mongodb][:dbpath] = "/vol/mongodb"
-#set[:mongodb][:journalpath] = "/vol/mongodb/journal"
-set[:mongodb][:use_fqdn] = false
+node.set[:mongodb][:smallfiles] = true
+#node.set[:mongodb][:dbpath] = "/vol/mongodb"
+#node.set[:mongodb][:journalpath] = "/vol/mongodb/journal"
+node.set[:mongodb][:use_fqdn] = false
 
 include_recipe "mongodb::mongodb"

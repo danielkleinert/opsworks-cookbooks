04010094478a44a4cf9135f018caa46dc8c0fa57
Daniel Kleinert <herr.kleinert@googlemail.com>
2013-07-15 12:05:36 +0200

    fix

--- flarebook/recipes/deploy_gameserver.rb
+++ flarebook/recipes/deploy_gameserver.rb
@@ -3,18 +3,21 @@ include_recipe "flarebook::update_gameserver_config"
 deploy = node[:deploy][:gameserver]
 application = :gameserver
 
-if deploy[:auto_npm_install_on_deploy]
-	OpsWorks::NodejsConfiguration.npm_install(application, node[:deploy][application], "#{deploy[:deploy_to]}/current/src/nodejs/")
-end
-
 link "#{deploy[:deploy_to]}/current/src/nodejs/config.json" do
   	to "#{deploy[:deploy_to]}/shared/config/gameserver_config.json"
 end
 
+link "#{deploy[:deploy_to]}/current/src/nodejs/log/" do
+  	to "#{deploy[:deploy_to]}/shared/log/"
+end
+
 ruby_block "restart node.js application #{application}" do
 	block do
-	  Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
-	  Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
-	  $? == 0
+		if deploy[:auto_npm_install_on_deploy]
+			OpsWorks::NodejsConfiguration.npm_install(application, node[:deploy][application], "#{deploy[:deploy_to]}/current/src/nodejs")
+		end
+		Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
+		Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
+		$? == 0
 	end	
 end

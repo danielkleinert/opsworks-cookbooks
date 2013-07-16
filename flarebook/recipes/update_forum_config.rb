721c33cbbea49df24e83187cbdddd90d7520b2b5
Daniel Kleinert <herr.kleinert@googlemail.com>
2013-07-16 10:41:43 +0200

    syntax error

--- flarebook/recipes/update_forum_config.rb
+++ flarebook/recipes/update_forum_config.rb
@@ -1,25 +1,29 @@
-include_recipe 'apache2::service'
 
-deploy = node[:deploy][:forum]
-application = :forum
+#if node[:deploy].attribute?(:forum)
 
-template "#{deploy[:deploy_to]}/shared/config/Settings.php" do
-    source 'forum_settings.php.erb'
-    mode '0660'
-    owner deploy[:user]
-    group deploy[:group]
-    variables(
-    	:mbname => node[:forum][:mbname],
-		:language => node[:forum][:language],
-		:boardurl => node[:forum][:boardurl],
-		:webmaster_email => node[:forum][:webmaster_email],
-		:db_server => deploy[:database][:host],
-		:db_name => deploy[:database][:database],
-		:db_user => deploy[:database][:username],
-		:db_passwd => deploy[:database][:password],
-		:db_mongo_server => "",
-		:db_mongo_db => "",
-		:db_mongo_replset => node[:mongodb][:replicaset_name],
-	)
-	notifies :restart, resources(:service => 'apache2'), :delayed
-end
+	include_recipe 'apache2::service'
+
+	deploy = node[:deploy][:forum]
+
+	template "#{deploy[:deploy_to]}/shared/config/Settings.php" do
+	    source 'forum_settings.php.erb'
+	    mode '0660'
+	    owner deploy[:user]
+	    group deploy[:group]
+	    variables(
+	    	:mbname => node[:forum][:mbname],
+			:language => node[:forum][:language],
+			:boardurl => node[:forum][:boardurl],
+			:webmaster_email => node[:forum][:webmaster_email],
+			:db_server => deploy[:database][:host],
+			:db_name => deploy[:database][:database],
+			:db_user => deploy[:database][:username],
+			:db_passwd => deploy[:database][:password],
+			:db_mongo_server => "",
+			:db_mongo_db => "",
+			:db_mongo_replset => node[:mongodb][:replicaset_name]
+		)
+		notifies :restart, resources(:service => 'apache2'), :delayed
+	end
+
+#end

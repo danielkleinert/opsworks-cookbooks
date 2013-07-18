4a1ae9ccedaf03fa281eed9014939e872935d8eb
Daniel Kleinert <herr.kleinert@googlemail.com>
2013-07-18 18:11:27 +0200

    bdgv

--- cloudwatch/attributes/defaults.rb
+++ cloudwatch/attributes/defaults.rb
@@ -6,4 +6,4 @@ default[:cloudwatch][:namespace] = node[:opsworks][:stack][:name]
 default[:cloudwatch][:monitor_memcached] = false
 default[:cloudwatch][:monitor_mongo] = node[:opsworks][:instance][:layers].include?('mongo')
 default[:cloudwatch][:monitor_mem] = true
-default[:cloudwatch][:disk_usage_paths] = Pathname.new("/vol").children.select { |c| c.directory? }.collect { |p| p.to_s }
+default[:cloudwatch][:disk_usage_paths] = []
